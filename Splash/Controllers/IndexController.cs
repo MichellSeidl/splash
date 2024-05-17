366bb413b3123d9965a758b2cb33867c9b45adf6
Fernando Oliveira <082200028@faculdade.cefsa.edu.br>
2024-05-17 01:26:41 -0300

    Adding Azure connection + Password Hash

--- Splash/Controllers/IndexController.cs
+++ Splash/Controllers/IndexController.cs
@@ -1,28 +1,64 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Splash._Utilities;
 using Splash.Models;
+using System.Data.SqlClient;
 
 namespace Splash.Controllers
 {
-	public class IndexController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public class IndexController : Controller
+    {
+        private readonly IConfiguration _configuration;
 
-		[HttpPost]
-		[Route("/ValidateUser")]
-		public IActionResult ValidateUser([FromBody] LoginViewModel user)
-		{
-			if (user.username != "michell")
-				return Json(new { isValid = false });
+        public IndexController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
-            if (user.password != "12345")
-                return Json(new { isValid = false });
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/ValidateUser")]
+        public IActionResult ValidateUser([FromBody] LoginViewModel user)
+        {
+
+            string query = "select * from usuarios where usuario= '" + user.username + "'";
+
+            //DatabaseConnector connector = new DatabaseConnector(_configuration);
+            //connector.ConnectToAzureSQL();
+
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            string hashedPassword = PasswordHasher.HashPassword(user.password);
+                            //while (reader.Read())
+                            //{
+                            string senha_db = reader.GetString(reader.GetOrdinal("senha"));
+                            if (senha_db != hashedPassword)
+                                return Json(new { isValid = false });
+                            //}
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
 
             return Json(new { isValid = true });
-		}
+        }
     }
 }
