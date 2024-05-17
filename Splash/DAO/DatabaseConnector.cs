2a412ad68f0dcf78bb2d25ea1421b3177f900286
Fernando Oliveira <082200028@faculdade.cefsa.edu.br>
2024-05-17 01:26:55 -0300

    Adding Azure connection + Password Hash

---
+++ Splash/DAO/DatabaseConnector.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace Splash.DAO
+{
+    public class DatabaseConnector
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseConnector(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConnectToAzureSQL()
+        {
+            string? azureConnectionString = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+
+            using (SqlConnection connection = new SqlConnection(azureConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection to Azure SQL Database opened successfully.");
+                    // You can perform database operations here
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    // Handle the exception appropriately
+                }
+            }
+        }
+    }
+}
